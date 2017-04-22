007bb04a3ea58e192f312635ab138cff1ea74618
keisatou <dummy>
2017-04-22 22:05:22 +0900

    add a local directive: v-local-highlight

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,8 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <!-- <p v-highlight:background="'red'">Color this</p> -->
-        <p v-highlight.delayed="'red'">Color this</p>
+        <p v-highlight:background.delayed="'red'">Color this</p>
+        <p v-local-highlight.delayed="'red'">Color this, too</p>
       </div>
     </div>
   </div>
@@ -20,6 +21,23 @@
 
 <script>
 export default {
+  directives: {
+    'local-highlight': {
+      bind(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers.delayed) {
+          delay = 3000;
+        }
+        setTimeout(() => {
+          if (binding.arg === 'background') {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, delay);
+      },
+    },
+  },
 };
 </script>
 
